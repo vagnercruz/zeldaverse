82ad14771a32c6d86d448f9986011e25c6b07490
vagnercruz <vagnerdacruz48@gmail.com>
2025-06-10 22:21:04 -0300

    adicionado a rota dos monstros

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Games from "../pages/Games";
+import Monsters from "../pages/Monsters"
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/jogos" element={<Games />} />
+      <Route path="/monstros" element={<Games />} />
     </Routes>
   );
 }
